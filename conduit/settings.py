45a9d1d65a7d828e93bfb1d39404e493d8a8eb99
Makoto Inoue <makoto.inoue@polytechs.co.jp>
2020-10-17 17:05:17 +0900

    stopped right before adding new users
    saved changes up to this point.

--- conduit/settings.py
+++ conduit/settings.py
@@ -88,6 +88,12 @@ DATABASES = {
     }
 }
 
+# Tell Django about the custom `User` model we created. The string
+# `authentication.User` tells Django we are referring to the `User` model in
+# the `authentication` module. This module is registered above in a setting
+# called `INSTALLED_APPS`.
+AUTH_USER_MODEL = 'authentication.User'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
